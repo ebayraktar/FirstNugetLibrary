808816d87b1268db3d04367aa6dece286caf03e1
Emre Bayraktar <emrebyrktr54@gmail.com>
2020-09-04 00:59:02 +0300

    Add project files.

---
+++ FirstNugetLibrary/FirstClass.cs
@@ -0,0 +1,30 @@
+﻿using Android.Content;
+using Android.Widget;
+
+namespace FirstNugetLibrary
+{
+    /// <summary>
+    /// First Class For Test
+    /// </summary>
+    public class FirstClass
+    {
+        /// <summary>
+        /// Test Class
+        /// </summary>
+        public FirstClass()
+        {
+            
+        }
+
+        /// <summary>
+        /// Prints the message
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="message"></param>
+        public void Print(Context context, string message)
+        {
+            Toast.MakeText(context, message, ToastLength.Short).Show();
+        }
+
+    }
+}
